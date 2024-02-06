66cc9a498c2481d3f549fda2e9f437f1c1bc2009
lohielisabeth <ellipetti01@hotmail.com>
2024-02-06 11:20:53 +0200

    Teht5 valmis

---
+++ Teht5/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "asunto.h"
+#include "katutaso.h"
+#include "kerrostalo.h"
+using namespace std;
+
+int main()
+{
+    kerrostalo* asukas4;
+
+    asukas4 = new kerrostalo;
+
+    asukas4->laskeKulutus(1);
+
+    delete asukas4;
+
+    return 0;
+}
